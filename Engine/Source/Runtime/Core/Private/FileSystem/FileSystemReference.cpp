c0a96d1c466ba33eef92c5f913a79e5a29ce1392
liberty <liberty@clover.games>
2021-04-05 00:36:38 +0900

    파일 시스템 작성 중

---
+++ Engine/Source/Runtime/Core/Private/FileSystem/FileSystemReference.cpp
@@ -0,0 +1,93 @@
+﻿// Copyright 2020-2021 Aumoa.lib. All right reserved.
+
+#include "FileSystem/FileSystemReference.h"
+
+#include "Core/String.h"
+
+using namespace std;
+using namespace std::filesystem;
+
+SFileSystemReference::SFileSystemReference(SString* inPath) : Super()
+{
+	myPath = inPath;
+}
+
+SFileSystemReference::~SFileSystemReference()
+{
+
+}
+
+SString* SFileSystemReference::GetRelativePath(SFileSystemReference* inReference) const
+{
+	// TODO:
+	return nullptr;
+}
+
+SString* SFileSystemReference::GetFullPath(SString* path)
+{
+	// TODO:
+	SString* currentDirectory = GetCurrentDirectory();
+	return nullptr;
+}
+
+SString* SFileSystemReference::GetCurrentDirectory()
+{
+	return new SString(current_path().c_str());
+}
+
+bool SFileSystemReference::IsValidPath(SString* path)
+{
+	// TODO:
+	return true;
+}
+
+bool SFileSystemReference::IsRootBased(SString* path)
+{
+	// C:\\ 등, 드라이브 문자가 포함된 경로의 최소 길이는 3글자입니다.
+	if (path->GetLength() < 3)
+	{
+		return false;
+	}
+
+	const wchar_t* c_str = path->C_Str();
+
+	if (!IsDriveLetter(c_str[0]))
+	{
+		return false;
+	}
+
+	if (c_str[1] != L':')
+	{
+		return false;
+	}
+
+	return !IsDirectorySeparator(c_str[2]);
+}
+
+bool SFileSystemReference::IsDriveLetter(wchar_t letter)
+{
+	return isalpha((int)letter) != 0;
+}
+
+bool SFileSystemReference::IsDirectorySeparator(wchar_t letter)
+{
+	return letter == DirectorySeparatorChar;
+}
+
+bool SFileSystemReference::IsValidPathChar(wchar_t letter, wchar_t additionalAllow)
+{
+	for (size_t i = 0; i < _countof(NotAllowedPathChars); ++i)
+	{
+		if (NotAllowedPathChars[i] == additionalAllow)
+		{
+			continue;
+		}
+
+		if (letter == NotAllowedPathChars[i])
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
---
+++ Engine/Source/Runtime/Core/Public/FileSystem/FileSystemReference.h
@@ -0,0 +1,59 @@
+﻿// Copyright 2020-2021 Aumoa.lib. All right reserved.
+
+#pragma once
+
+#include "Core/Object.h"
+
+class SString;
+
+/// <summary>
+/// 파일 시스템에서 특정 경로에 대한 레퍼런스를 탐색합니다.
+/// </summary>
+SCLASS(SFileSystemReference) class SFileSystemReference : virtual public SObject
+{
+	SCLASS_BODY(SFileSystemReference)
+
+private:
+	static constexpr const wchar_t NotAllowedPathChars[] = L"\\/:*?\"<>|";
+	static constexpr const wchar_t DirectorySeparatorChar = L'\\';
+
+	SPROPERTY(SString*, myPath);
+
+public:
+	/// <summary>
+	/// 상대적 경로 또는 전체 경로를 지정하여 새 레퍼런스를 생성합니다.
+	/// </summary>
+	/// <param name="inPath"> 지원하는 경로 유형을 전달합니다. </param>
+	SFileSystemReference(SString* inPath);
+	~SFileSystemReference() override;
+
+	/// <summary>
+	/// 대상 레퍼런스와 상대적 경로를 가져옵니다.
+	/// </summary>
+	/// <param name="inReference"> 대상 레퍼런스를 전달합니다. </param>
+	/// <returns> 상대적 경로가 반환됩니다. </returns>
+	SString* GetRelativePath(SFileSystemReference* inReference) const;
+
+protected:
+	/// <summary>
+	/// 상대 경로의 절대 경로를 가져옵니다.
+	/// </summary>
+	/// <param name="path"> 상대 경로를 전달합니다. </param>
+	/// <returns> 절대 경로가 반환됩니다. </returns>
+	static SString* GetFullPath(SString* path);
+
+	/// <summary>
+	/// 시스템의 현재 디렉토리를 가져옵니다.
+	/// </summary>
+	/// <returns> 현재 디렉토리 경로가 반환됩니다. </returns>
+	static SString* GetCurrentDirectory();
+
+private:
+	static bool IsValidPath(SString* path);
+	static bool IsRootBased(SString* path);
+	static void NormalizePathDirectorySeparator(SString* path);
+
+	static bool IsDriveLetter(wchar_t letter);
+	static bool IsDirectorySeparator(wchar_t letter);
+	static bool IsValidPathChar(wchar_t letter, wchar_t additionalAllow);
+};
